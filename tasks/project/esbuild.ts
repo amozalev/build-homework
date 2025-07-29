c21a9e04bdd43641866543773f1cf7c984579c6e
amozalev <mozalev.alexey@gmail.com>
2025-07-29 22:03:16 +0300

    Implemented esbuild bundle in the 5th task.

--- tasks/project/esbuild.ts
+++ tasks/project/esbuild.ts
@@ -1,7 +1,46 @@
+import fs from "node:fs";
 import esbuild from 'esbuild';
+import {htmlPlugin} from '@craftamap/esbuild-plugin-html';
 
 const options = {
+    entryPoints: ["./src/index.tsx"],
+    bundle: true,
+    metafile: true,
+    outdir: './dist/esbuild',
+    tsconfig: "tsconfig.json",
+    publicPath: '/esbuild/',
+    jsx: "automatic",
+    loader: {
+        ".ts": "ts",
+        ".tsx": "tsx",
+        ".css": "css",
+        ".html": "copy"
+    },
+    plugins: [
+        htmlPlugin({
+            files: [
+                {
+                    entryPoints: ["src/index.tsx"],
+                    scriptLoading: 'defer',
+                    title: 'App by Esbuild',
+                    filename: 'index.html',
+                    htmlTemplate: `
+                    <!DOCTYPE html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8" />
+                      </head>
+                      <body>
+                      <div id="root"></div>
+                      </body>
+                    </html>
+                `
+                }
+            ]
+        })
+    ]
+};
 
-}
-
-esbuild.build(options).catch(() => process.exit(1))
+(async function () {
+    await esbuild.build(options).catch(e => console.log('==', e));
+})();
