19fba9ebfee74efe163f16f05ecadfd1eaad6de9
amozalev <mozalev.alexey@gmail.com>
2025-08-25 22:05:46 +0300

    Implemented 10th task.

--- tasks/10/watch.js
+++ tasks/10/watch.js
@@ -1,9 +1,27 @@
+import chokidar from 'chokidar';
+import fg from 'fast-glob';
+
 const changedFilesLog = [];
+let watcher;
 
 export function getLog() {
-  return changedFilesLog;
+  const set = new Set();
+  return changedFilesLog.filter(path => {
+    if (set.has(path)) {
+      return false;
+    }
+    set.add(path);
+    return true;
+  })
 }
 
-export async function subscribe() {}
+export async function subscribe() {
+  const files = await fg('src/**/*.js');
+  watcher = await chokidar.watch(files).on('change', (event, path) => {
+      changedFilesLog.push(event);
+  });
+}
 
-export function unsubscribe() {}
+export async function unsubscribe() {
+  await watcher.close().then(() => console.log('== Watcher closed'));
+}
